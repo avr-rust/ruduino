99ad3f0ab116a12f7fd4447e3685d9340950134b
Jake Goulding <jake.goulding@gmail.com>
2017-07-15 22:49:22 -0400

    Expose some structs for the IO ports

---
+++ src/io/mod.rs
@@ -0,0 +1,5 @@
+pub mod port;
+
+pub const PORT_B: port::Port<port::B> = port::Port::new();
+pub const PORT_C: port::Port<port::C> = port::Port::new();
+pub const PORT_D: port::Port<port::D> = port::Port::new();
