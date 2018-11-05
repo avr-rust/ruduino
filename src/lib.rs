3fbe33504ac57f9e8b06c561992281f25a8196c8
Dylan McKay <me@dylanmckay.io>
2018-11-05 23:35:46 +1300

    Fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! Definitions of register addresses and bits within those registers
 
 #![feature(asm)]
-#![feature(no_core)]
 #![feature(const_fn)]
 #![feature(associated_consts)]
 #![feature(associated_type_defaults)]
@@ -9,10 +8,6 @@
 #![feature(lang_items)]
 #![feature(panic_handler)]
 #![feature(unwind_attributes)]
-
-#![no_core]
-
-#![no_std]
 
 pub use self::register::{Register, RegisterBits, RegisterValue};
 pub use self::pin::{DataDirection, Pin};
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,6 @@
 //! Re-exports commonly-used APIs that can be imported at once.
 
+use core::prelude::v1::*;
+
 pub use interrupt::without_interrupts;
 
