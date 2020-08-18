95d05a627739e0b66bc9f189953fa881b06403c2
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2020-08-17 21:36:07 -0400

    Export ClockSource and WaveformGenerationMode for each timer

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,7 +1,10 @@
 //! Modules that can be implemented for specific cores.
 
 pub use self::spi::HardwareSpi;
-pub use self::timer::{Timer8, Timer8Setup, Timer16, Timer16Setup};
+pub use self::timer::{
+    Timer8, Timer8Setup, ClockSource8, WaveformGenerationMode8,
+    Timer16, Timer16Setup, ClockSource16, WaveformGenerationMode16,
+};
 pub use self::usart::HardwareUsart;
 
 mod spi;
--- src/modules/timer/mod.rs
+++ src/modules/timer/mod.rs
@@ -1,6 +1,11 @@
-pub use self::timer8::{Timer8, Timer8Setup};
-pub use self::timer16::{Timer16, Timer16Setup};
+pub use self::timer8::{
+    ClockSource as ClockSource8, Timer8, Timer8Setup,
+    WaveformGenerationMode as WaveformGenerationMode8,
+};
+pub use self::timer16::{
+    ClockSource as ClockSource16, Timer16, Timer16Setup,
+    WaveformGenerationMode as WaveformGenerationMode16,
+};
 
 mod timer8;
 mod timer16;
-
