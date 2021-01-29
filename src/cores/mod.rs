8242dc880f99e16672d2a07fa9113ae96eea6dbc
Dylan McKay <me@dylanmckay.io>
2021-01-29 20:17:48 +1300

    Run re-generate MCU cores script

--- src/cores/mod.rs
+++ src/cores/mod.rs
@@ -1,59 +1,61 @@
 //! The primary module containing microcontroller-specific core definitions
 
 /// The ATmega88.
-pub mod atmega88;
+#[cfg(any(avr_mcu_atmega88, feature = "all-mcus"))] pub mod atmega88;
 #[cfg(avr_mcu_atmega88)] pub use self::atmega88 as current;
 
 /// The ATmega48A.
-pub mod atmega48a;
+#[cfg(any(avr_mcu_atmega48a, feature = "all-mcus"))] pub mod atmega48a;
 #[cfg(avr_mcu_atmega48a)] pub use self::atmega48a as current;
 
 /// The ATmega168A.
-pub mod atmega168a;
+#[cfg(any(avr_mcu_atmega168a, feature = "all-mcus"))] pub mod atmega168a;
 #[cfg(avr_mcu_atmega168a)] pub use self::atmega168a as current;
 
 /// The ATmega88P.
-pub mod atmega88p;
+#[cfg(any(avr_mcu_atmega88p, feature = "all-mcus"))] pub mod atmega88p;
 #[cfg(avr_mcu_atmega88p)] pub use self::atmega88p as current;
 
 /// The ATmega168P.
-pub mod atmega168p;
+#[cfg(any(avr_mcu_atmega168p, feature = "all-mcus"))] pub mod atmega168p;
 #[cfg(avr_mcu_atmega168p)] pub use self::atmega168p as current;
 
 /// The ATmega88PA.
-pub mod atmega88pa;
+#[cfg(any(avr_mcu_atmega88pa, feature = "all-mcus"))] pub mod atmega88pa;
 #[cfg(avr_mcu_atmega88pa)] pub use self::atmega88pa as current;
 
 /// The ATmega168.
-pub mod atmega168;
+#[cfg(any(avr_mcu_atmega168, feature = "all-mcus"))] pub mod atmega168;
 #[cfg(avr_mcu_atmega168)] pub use self::atmega168 as current;
 
 /// The ATmega328P.
-pub mod atmega328p;
+#[cfg(any(avr_mcu_atmega328p, feature = "all-mcus"))] pub mod atmega328p;
 #[cfg(avr_mcu_atmega328p)] pub use self::atmega328p as current;
 
 /// The ATmega48PA.
-pub mod atmega48pa;
+#[cfg(any(avr_mcu_atmega48pa, feature = "all-mcus"))] pub mod atmega48pa;
 #[cfg(avr_mcu_atmega48pa)] pub use self::atmega48pa as current;
 
 /// The ATmega168PA.
-pub mod atmega168pa;
+#[cfg(any(avr_mcu_atmega168pa, feature = "all-mcus"))] pub mod atmega168pa;
 #[cfg(avr_mcu_atmega168pa)] pub use self::atmega168pa as current;
 
 /// The ATmega48P.
-pub mod atmega48p;
+#[cfg(any(avr_mcu_atmega48p, feature = "all-mcus"))] pub mod atmega48p;
 #[cfg(avr_mcu_atmega48p)] pub use self::atmega48p as current;
 
 /// The ATmega328.
-pub mod atmega328;
-#[cfg(avr_mcu_atmega328)] pub use self::atmega328 as current;
+///
+/// This device is chosen as the default when the crate is targeting non-AVR devices.
+#[cfg(any(avr_mcu_atmega328, feature = "all-mcus", not(target_arch = "avr")))] pub mod atmega328;
+#[cfg(any(avr_mcu_atmega328, not(target_arch = "avr")))] pub use self::atmega328 as current;
 
 /// The ATmega88A.
-pub mod atmega88a;
+#[cfg(any(avr_mcu_atmega88a, feature = "all-mcus"))] pub mod atmega88a;
 #[cfg(avr_mcu_atmega88a)] pub use self::atmega88a as current;
 
 /// The ATmega48.
-pub mod atmega48;
+#[cfg(any(avr_mcu_atmega48, feature = "all-mcus"))] pub mod atmega48;
 #[cfg(avr_mcu_atmega48)] pub use self::atmega48 as current;
 
 
