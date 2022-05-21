ed0b92131adab617fa9775ad0bfe208d9d1f6d0e
Michael Buesch <m@bues.ch>
2022-05-21 19:41:30 +0200

    irqs: Add helper to restore interrupts

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -3,21 +3,31 @@
 use core::prelude::v1::*;
 use core::marker::PhantomData;
 use core::arch::asm;
+use crate::cores::current::SREG;
+use crate::register::Register;
 
-/// Helper struct that automatically restores interrupts
+/// Helper struct that automatically enables interrupts
 /// on drop.
 struct DisableInterrupts(PhantomData<()>);
 
 /// Executes a closure, disabling interrupts until its completion.
 ///
-/// Restores interrupts after the closure has completed
+/// Unconditionally enables interrupts after the closure has completed
 /// execution.
 #[inline(always)]
-pub fn without_interrupts<F, T>(f: F) -> T
+pub fn with_irqs_disabled<F, T>(f: F) -> T
     where F: FnOnce() -> T
 {
     let _disabled = DisableInterrupts::new();
     f()
+}
+
+#[inline(always)]
+#[deprecated(note="Please use with_irqs_disabled instead")]
+pub fn without_interrupts<F, T>(f: F) -> T
+    where F: FnOnce() -> T
+{
+    with_irqs_disabled(f)
 }
 
 impl DisableInterrupts {
@@ -35,3 +45,34 @@ impl Drop for DisableInterrupts {
     }
 }
 
+/// Helper struct that automatically restores interrupts
+/// on drop.
+struct SuspendInterrupts(u8);
+
+/// Executes a closure, disabling interrupts until its completion.
+///
+/// Restores interrupts to the previous state after the closure has completed
+/// execution.
+#[inline(always)]
+pub fn with_irqs_suspended<F, T>(f: F) -> T
+    where F: FnOnce() -> T
+{
+    let _disabled = SuspendInterrupts::new();
+    f()
+}
+
+impl SuspendInterrupts {
+    #[inline(always)]
+    pub fn new() -> SuspendInterrupts {
+        let ret = SuspendInterrupts(SREG::read());
+        unsafe { asm!("cli") }
+        ret
+    }
+}
+
+impl Drop for SuspendInterrupts {
+    #[inline(always)]
+    fn drop(&mut self) {
+        SREG::write(self.0);
+    }
+}
